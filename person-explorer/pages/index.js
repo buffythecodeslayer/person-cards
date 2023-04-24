28c850e01d1998f5a7f944d7488c78f301aee1b6
Dan <danhuynh700101@gmail.com>
2023-04-23 21:23:21 -0400

    Add ApolloClient

--- person-explorer/pages/index.js
+++ person-explorer/pages/index.js
@@ -1,5 +1,16 @@
-function HomePage() {
-    return <div>Welcome to Next.js!</div>
-};
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const DEFAULT_SWAPI_GRAPHQL_URI = 'http://localhost:4000/';
+
+const client = new ApolloClient({
+    uri: DEFAULT_SWAPI_GRAPHQL_URI,
+    cache: new InMemoryCache(),
+});
+
+const HomePage = () => (
+    <ApolloProvider client={client}>
+        <div>Welcome to Next.js!</div>
+    </ApolloProvider>
+);
 
 export default HomePage;
