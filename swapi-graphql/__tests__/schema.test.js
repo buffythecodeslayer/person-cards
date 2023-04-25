677d58cff3d385bbc69618dff8adfa9d33964e3e
Dan <danhuynh700101@gmail.com>
2023-04-25 12:39:41 -0400

    Update unit tests to include homeworld query

--- swapi-graphql/__tests__/schema.test.js
+++ swapi-graphql/__tests__/schema.test.js
@@ -13,13 +13,28 @@ const mockResponse = {
     count: 1,
     next: null,
     results: [{
-        name: "Luke Skywalker",
-        height: "172",
-        mass: "77",
-        birth_year: "19BBY",
-        homeworld: "https://swapi.dev/api/planets/1/",
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        birth_year: '19BBY',
+        origin: 'Tatooine',
         },
 ]};
+
+const mockPeopleResponse = {
+    ...mockResponse,
+    results: [{
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        birth_year: '19BBY',
+        homeworld: 'https://swapi.dev/api/planets/1/',
+        },
+]};
+
+const mockHomeworldResponse = {
+    name: 'Tatooine',
+}
 
 const query = gql`
     query GetPeople ($page: Int!) {
@@ -31,7 +46,7 @@ const query = gql`
                 height
                 mass
                 birth_year
-                homeworld
+                origin
             }
         }
     }
@@ -50,7 +65,9 @@ describe('people resolver', () => {
     const contextValue = { dataSources: { starwarsApi } };
 
     test('returns people data', async () => {
-        starwarsApi.get = jest.fn(() => mockResponse);
+        starwarsApi.get = jest.fn()
+            .mockReturnValueOnce(mockPeopleResponse)
+            .mockReturnValueOnce(mockHomeworldResponse);
         const res = await server.executeOperation(
             { query, variables },
             { contextValue },
@@ -60,10 +77,30 @@ describe('people resolver', () => {
         expect(res.body.singleResult.data.people).toEqual(mockResponse);
     });
 
-    test('should throw an error if the Starwars Api request fails', async () => {
+    test('should throw an error if the Starwars Api people request fails', async () => {
         starwarsApi.get = jest.fn(() => {
             throw new Error('Error in HTTP Request/Response');
-        });
+            });
+        const res = await server.executeOperation(
+            { query, variables },
+            { contextValue },
+        );
+
+        /**
+         * Note that when testing, any errors in parsing, validating, and executing your GraphQL
+         * operation are returned in the nested errors field of the result. As with any GraphQL
+         * response, these errors are not thrown.
+         * https://www.apollographql.com/docs/apollo-server/testing/testing/#executing-queries-and-mutations
+         */
+        expect(res.body.singleResult.errors).toBeDefined();
+    });
+
+    test('should throw an error if the Starwars Api homeworld request fails', async () => {
+        starwarsApi.get = jest.fn()
+            .mockReturnValueOnce(mockPeopleResponse)
+            .mockImplementationOnce(() => {
+                throw new Error('Error in HTTP Request/Response');
+            });
         const res = await server.executeOperation(
             { query, variables },
             { contextValue },
